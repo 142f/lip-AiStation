461ad0191eef6f656a888d569072dafd4c47b3aa
14259 <1425912243@qq.com>
2025-10-11 23:26:02 +0800

    修改了不可以使用class_bal类别平衡器的错误

--- data/__init__.py
+++ data/__init__.py
@@ -28,12 +28,19 @@ def create_dataloader(opt):
     dataset = AVLip(opt)
 
     sampler = get_bal_sampler(dataset) if opt.class_bal else None
+    # 若传入 sampler，则 DataLoader 的 shuffle 必须为 False
+    if sampler is not None:
+        shuffle = False
+        print("使用类别均衡采样器，已将 shuffle 设为 False。")
 
     data_loader = torch.utils.data.DataLoader(
         dataset,
         batch_size=opt.batch_size,
-        shuffle=True,
+        shuffle=shuffle,   # 使用计算好的 shuffle（而不是硬编码 True）
         sampler=sampler,
         num_workers=int(opt.num_threads),
     )
+    print("DataLoader 创建完成：batch_size={}, shuffle={}, sampler={}".format(
+        opt.batch_size, shuffle, "有" if sampler is not None else "无"
+    ))
     return data_loader
