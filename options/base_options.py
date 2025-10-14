9a186f5e1ccf33dbfab77a3f7747370ec705f538
14259 <1425912243@qq.com>
2025-10-15 00:18:10 +0800

    命令行参数逻辑修改

--- options/base_options.py
+++ options/base_options.py
@@ -23,7 +23,7 @@ class BaseOptions:
         parser.add_argument("--arch", type=str, default="CLIP:ViT-L/14", help="Model architecture. See models/__init__.py for options.")
         parser.add_argument("--batch_size", type=int, default=10, help="Input batch size for training.")
         parser.add_argument("--fix_backbone", action="store_true", help="If specified, freezes the backbone weights during training.")
-        parser.add_argument("--fix_encoder", action="store_true", default=True, help="If specified, freezes the encoder weights during training.")
+        parser.add_argument("--fix_encoder", action="store_true",help="If specified, freezes the encoder weights during training.")
         parser.add_argument("--serial_batches", action="store_true", help="If true, takes images in order to make batches, otherwise takes them randomly.")
         
         # ===================================================================
